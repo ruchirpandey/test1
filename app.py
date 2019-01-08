96fcb26df31b3589aa5cf89524a4d2c55f394cf4
ruchirpandey <ruchir.pandey@gmail.com>
2019-01-08 22:42:32 +0530

    new
    new

--- app.py
+++ app.py
@@ -16,25 +16,7 @@ app = Flask(__name__)
 def hello():
     print("Hello I am here")
     return "Hello World!"
-from flask import Flask
- 
-class ReusableForm(Form):
-      name = TextField('Name:', validators=[validators.required()])
- 
-@app.route("/form", methods=['GET', 'POST'])
-def hello1():
-    form = ReusableForm(request.form)
-    print form.errors
-    if request.method == 'POST':
-       name=request.form['name']
-       print name
- 
-    if form.validate():
-# Save the comment here.
-       flash('Hello ' + name)
-    else:
-       flash('All the form fields are required. ')
-    return render_template('hello.html', form=form)
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
