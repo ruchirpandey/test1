d4d3a20679502502cc252f88bd26c0c98077d4d5
Ruchir Pandey <ruchir.pandey@gmail.com>
2021-07-26 10:44:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     print("Hello Ittsts am here")
-    return "Hello World! Iaskalsakslakls am auto deployed"
+    return "Hello World! See I got Auto deployed"
 
 @app.route('/sachin')
 def sachin():
